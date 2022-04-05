614a58ce9a63e93a2c164a4c0a61c0a8a89e6984
Nicholas Piël <nicholas@surfly.com>
2022-04-05 17:18:52 +0200

    Create chatbot2.js

---
+++ chatbot2.js
@@ -0,0 +1,7 @@
+    window.__be = window.__be || {};
+    window.__be.id = "624c47ee9945060007108d2e";
+    (function() {
+        var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
+        be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+    })();
