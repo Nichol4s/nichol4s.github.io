62865b1e4f90db309d03640050049e0689b489cb
Nicholas Piël <nicholas@surfly.com>
2022-04-06 13:34:35 +0200

    Update chatbot2.js

--- chatbot2.js
+++ chatbot2.js
@@ -1,3 +1,4 @@
+
     window.__be = window.__be || {};
     window.__be.id = "624c47ee9945060007108d2e";
     (function() {
@@ -5,3 +6,5 @@
         be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
     })();
+
+window.addEventListener("load", function() {  document.querySelector(".c-quick-launcher").style.display = "none";} );
